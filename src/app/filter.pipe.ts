fc8f1732aafc58f44ad4116df451b5dc83010383
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-06-09 04:31:16 +0200

    228 Example creating a filter pipe

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: any, propName: string): any {
+    if (value === null || filterString === '') {
+      return value;
+    }
+    const resultArray = [];
+    for (let item of value) {
+      if (item[propName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+  }
+
+}
